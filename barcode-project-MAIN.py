90fcff13600462648dde8d1f724960176a3689c2
aliciachin <34766719+aliciachin@users.noreply.github.com>
2021-02-25 12:27:47 +1100

    updating files in repo

---
+++ barcode-project-MAIN.py
@@ -0,0 +1,111 @@
+# Inventory program with barcode scanner
+"""Inventory of all food items in the kitchen. User will be able to add a new item,
+update an existing item or delete an item.
+
+Database will have the following columns:
+barcode, item, category, quantity, unit, expiry_date, date_added, last_updated
+
+
+FUNTIONALITY
+User will be prompted to select an option in the display menu; add, update (increase, decrease, correct), delete
+
+ERROR CHECKS
+Check for duplicate entries"""
+
+# Display menu
+'''Prompts the user to select an option; add, update (increase, decrease, correct), delete'''
+
+
+
+
+def display_menu():
+    # TODO: Set while loop before performing redirection
+    valid = False
+    while not valid:
+        option = input("Select an option:\n"
+                       "1 - Add to inventory\n"
+                       "2 - Record usage of an item\n"
+                       "3 - Change details of an existing item\n"
+                       "4 - Delete from inventory\n")
+        int_option = int(option)
+        if 1<=int_option<=4:
+            valid = True
+
+    # TODO: check user input is string non-numeric first and send error message before converting to integer
+            if option == 1:
+                print("Selected option: " + str(option))
+                new_item()
+            if option == 2:
+                print("Selected option: " + str(option))
+                decrease_item()
+            if option == 3:
+                print("Selected option: " + str(option))
+                edit_details()
+            if option == 4:
+                print("Selected option: " + str(option))
+                delete_item()
+
+
+
+'''def display_menu(option):
+    not_valid = True
+    while not_valid:
+        print('1,2,3,4')
+        option = int(input('12,,3,4)'))
+        if valid_option():
+            not_valid = False
+        else:
+            print('only select between 1 to 4 incl')
+    if option == 1:
+        do 1'''
+
+
+# ADD - Add to inventory
+# 1. Scan barcode
+# 2. If new, information required:
+#       - item (string)
+#       - category (multi-select)
+#       - quantity
+#       - unit (multi-select)
+#       - expiry_date (YYYY-MM-DD format)
+#       Add to database
+# 3. If existing, information required:
+#       -
+#
+
+def new_item():
+    barcode = input("Please scan the item's barcode. ")
+
+    # check that barcode is valid - must be a number.
+    while not barcode.isnumeric():
+        print("Barcode must be numeric.")
+        barcode = input("Please scan the item's barcode. ")
+
+    else :
+        pass
+
+
+'''If barcode exists in the database, user can update or delete.'''
+
+
+# UPDATE - Record usage of an item
+def decrease_item():
+    pass
+
+
+# EDIT - Change details of an existing item
+def edit_details():
+    pass
+
+
+# DELETE - Delete from inventory
+def delete_item():
+    pass
+
+display_menu()
+
+
+
+
+
+
