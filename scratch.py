44e1c7a84cad6e05513f0ba25f2e25ba97933ac1
aliciachin <34766719+aliciachin@users.noreply.github.com>
2021-03-03 08:58:45 +1100

    Resolved looping upon incorrect input in scratch.py

--- scratch.py
+++ scratch.py
@@ -1,19 +1,25 @@
 def display_menu():
-    option = input("Select an integer between 1 and 4 inclusive.\n")
-    try:
-        int_option = int(option)
-        if not 1 <= int_option <= 4:
-            print("1 to 4. There are only four options. How did you go wrong there?")
-        if int_option == 1:
-            print("Yay")
-        if int_option == 2:
-            print("Yay")
-        if int_option == 3:
-            print("Yay")
-        if int_option == 4:
-            print("Yay")
-    except ValueError:
-        print("I said an integer. You entered a string, bitch.")
+    valid = False
+    while not valid:
+        try:
+            option = input("Select an integer between 1 and 4 inclusive.\n")
+            int_option = int(option)
+            if not 1 <= int_option <= 4:
+                print("1 to 4. There are only four options. How did you go wrong there?")
+            if int_option == 1:
+                print("Yay")
+                valid = True
+            if int_option == 2:
+                print("Yay")
+                valid = True
+            if int_option == 3:
+                print("Yay")
+                valid = True
+            if int_option == 4:
+                print("Yay")
+                valid = True
+        except ValueError:
+            print("I said an integer. You entered a string, bitch.")
 
 
 display_menu()
