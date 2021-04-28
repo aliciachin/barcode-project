e4370d4acdc09628a0ab834b71ecb9eaf391a3ea
aliciachin <34766719+aliciachin@users.noreply.github.com>
2021-04-28 13:56:57 +1000

    Developed new_item() code; Created SQL queries to add new entries

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ User will be prompted to select an option in the display menu; add, update (incr
 ERROR CHECKS
 Check for duplicate entries"""
 
+import sqlite3
+
 
 def display_menu():
     # Prompts the user to select an option; add, update (increase, decrease, correct), delete
@@ -52,6 +54,23 @@ def display_menu():
         except ValueError:
             print("Please select a valid option: 1, 2, 3, 4\n")
 
+# Function to add entry to database
+def add_one(barcode,item,category,quantity,unit,expiry_date):
+    conn = sqlite3.connect('barcode.db')
+    c = conn.cursor()
+
+    c.execute('''
+    INSERT INTO sample VALUES
+        (?,?,?,?,?,?,datetime('now','localtime'),datetime('now','localtime'));''',(barcode,item,category,quantity,unit,expiry_date))
+    conn.commit()
+    conn.close()
+
+def show_all():
+    conn = sqlite3.connect('barcode.db')
+    c = conn.cursor()
+    c.execute('''SELECT * FROM sample;''')
+    conn.commit()
+    conn.close()
 
 def new_item():
     # ADD - Add to inventory
@@ -65,9 +84,54 @@ def new_item():
         if barcode.isnumeric():
             # Check that barcode is a number.
             valid = True
-            pass
 
-            # TODO: Write code for new entry - append to database
+
+            # Check if barcode exists in the database.
+            conn = sqlite3.connect('barcode.db')
+            c = conn.cursor()
+            c.execute('''SELECT id FROM sample WHERE id = (?)''',(barcode,))
+            result = c.fetchone()
+            conn.commit()
+            conn.close()
+
+
+            # If barcode does not exist, get details to create new entry.
+            if result is None:
+                print("Enter the following details.\n")
+                item = input("Item name: ")
+                category = input("Category: ")
+                unit = input("Unit (g or ml only): ")
+                quantity = input("Quantity: ")
+                expiry_date = input("Date of expiry (YYYY-MM-DD): ")
+
+                add_one(barcode,item,category,quantity,unit,expiry_date)
+
+                # TODO: show_all() doesn't work
+                show_all()
+
+
+            # If barcode exists, get quantity (and expiry date) to update details.
+            # TODO: Determine how to store same items with different expiry dates.
+            else:
+                # TODO: Retrieve entry to review details and capture unit
+                # print("Enter the following details.\n")
+                # quantity = input("Quantity (in " + str(unit) "): ")
+                # expiry_date = input("Date of expiry: ")
+
+                # def update(quantity,expiry_date):
+                #     conn = sqlite3.connect('barcode.db')
+                #     c = conn.cursor()
+                #
+                #     c.execute('''
+                #     UPDATE...''')
+                #
+                #     c.execute('''
+                #     SELECT * FROM sample''')
+                # update(quantity,expiry_date)
+                    pass
+
+
+
 
             # If barcode is not in database,
             # Get input:
@@ -78,7 +142,7 @@ def new_item():
             #   - expiry_date (YYYY-MM-DD format)
             # Add to database
 
-            # TODO: Write code for existing entry - update record in database
+            # TODO: In SQL, write code for existing entry - update record in database
             # If barcode exists in database,
             # Get input:
             #   - quantity (display current unit for user)
--- sql.py
+++ sql.py
@@ -1,32 +1,58 @@
 # Database will have the following columns:
 # id, item, category, quantity, unit, expiry_date, date_added, last_updated
 
-# UPDATE: Use sample2 for testing
+# UPDATE: test database is sample.db
 
 import sqlite3
 
-conn = sqlite3.connect('barcode.db')
-c = conn.cursor()
 
-# c.execute('''
-#     CREATE TABLE sample2(
-#         id INTEGER,
-#         item VARCHAR,
-#         category VARCHAR,
-#         quantity REAL,
-#         unit VARCHAR,
-#         expiry_date DATE,
-#         date_added DATETIME,
-#         last_updated DATETIME
-#         );''')
+# SQL query to create a sample database
+def create_sample_db():
+    conn = sqlite3.connect('barcode.db')
+    c = conn.cursor()
 
-# c.execute('''
-#     INSERT INTO sample2 VALUES
-#         ('12345678','item1','category1','100','ml','2000-01-01','2000-01-01 10:00:00','2000-01-01 10:00:05'),
-#         ('87654321','item2','category2','300','g','2000-01-20','2000-01-05 10:00:00','2000-01-07 10:00:00'),
-#         ('19273618','item3','category3','250','ml','2000-01-10','2000-01-02 10:00:00','2000-01-02 10:00:00')
-#         ;''')
-#
-# conn.commit()
+    c.execute('''
+        CREATE TABLE sample(
+            id INTEGER,
+            item VARCHAR,
+            category VARCHAR,
+            quantity REAL,
+            unit VARCHAR,
+            expiry_date DATE,
+            date_added DATETIME,
+            last_updated DATETIME
+            );''')
+
+    c.execute('''
+        INSERT INTO sample VALUES
+            ('12345678','item1','category1','100','ml','2000-01-01','2000-01-01 10:00:00','2000-01-01 10:00:05'),
+            ('87654321','item2','category2','300','g','2000-01-20','2000-01-05 10:00:00','2000-01-07 10:00:00'),
+            ('19273618','item3','category3','250','ml','2000-01-10','2000-01-02 10:00:00','2000-01-02 10:00:00')
+            ;''')
+
+    conn.commit()
+    conn.close()
+
+
+
+# SQL query to add a new item to the database
+def add_one(id,item,category,quantity,unit,expiry_date):
+    conn = sqlite3.connect('barcode.db')
+    c = conn.cursor()
+
+    c.execute('''
+    INSERT INTO sample VALUES
+        (id,item,category,quantity,unit,datetime('now'),datetime('now')),
+        ;''')
+
+
+c.execute('''
+        SELECT * FROM sample''')
+
+# print(c.fetchone()[1])
+# print(c.fetchmany())
+print(c.fetchall())
+
+conn.commit()
 conn.close()
 
